5194ee3e992927325619707a246c213e6e9bd421
Human Inhabitant <human-inhabitant@pm.me>
2020-08-20 09:51:08 -0400

    Completed chapter 02...

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+console.info('Hello TypeScript...');
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b
+};
+let e = '4';
+let d = c.apple + e;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+console.info( 'Hello TypeScript...' );
+
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+  apple: a,
+  banana: b
+}
+let e = '4';
+let d = c.apple + e;
